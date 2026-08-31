449091ba6e2f1862588b0605b7077763bef286c2
Antoine PIETRI <antpietri@IUT.BX>
2026-08-31 22:24:55 +0200

    v4.0.0 - refonte visuelle harmonisee des 3 pages : logo GMP a la meme hauteur (68px) et titres identiques (1.5rem/800) ; usinage sans bande blanche de toolbar (fond transparent, actions a droite) ; fabrication-additive passe en VIOLET (accents + fond, clair/sombre) et fonds de la visualisation 3D en violet adaptatifs au theme ; cache SW v2.0 -> v2.1

--- usinage/sw.js
+++ usinage/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reservation-gmp-v2.0';
+const CACHE = 'reservation-gmp-v2.1';
 const ASSETS = ['./', './index.html', './petitgmp.png', './BM_GMP_HD_1000.png', './manifest.json'];
 
 self.addEventListener('install', e => {
